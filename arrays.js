4cc441fcc968335573921fa155f3df554778297a
Jen W <jsc525@gmail.com>
2019-05-24 02:04:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,4 +8,6 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return [element,...array];
 }
 
-function 
+function addElementToBeginningOfArray(array, element) {
+  return array.push(element);
+}
